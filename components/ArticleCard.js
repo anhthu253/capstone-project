f32940c00bacfbc7fa4bea8cbbe427a8d3ee46cd
Thu Nguyen <thu_nguyen@gmx.de>
2022-10-18 16:46:53 +0200

    replace image with texts as dummy data. Testing with programmable router to switch between content and main site

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function ArticleCard({ imageUrl }) {
-  return <Image src={imageUrl} width={600} height={400}></Image>;
+export default function ArticleCard({ text }) {
+  const router = useRouter();
+  return (
+    <StyledCard onClick={() => router.push("/content")}>{text}</StyledCard>
+  );
 }
+
+const StyledCard = styled.section`
+  width: 400px;
+  height: 400px;
+  border: var(--line-secondary);
+  &:hover {
+    cursor: pointer;
+  }
+`;
--- components/ArticleListContainer.js
+++ components/ArticleListContainer.js
@@ -6,17 +6,28 @@ const initialArticles = [
   {
     id: "24343",
     imageUrl: "/images/image-item1.jpg",
-    text: "rgasgsdfasdfdsfgasdfsadfagsdrtreterwtrewtfasdfasdfdsgasgadga",
+    text: `CANBERRA, Australia — Australia has reversed a previous government’s recognition of West Jerusalem as Israel’s capital, the foreign minister said Tuesday.
+  
+      The center-left Labor Party government Cabinet agreed to again recognize Tel Aviv as the capital and reaffirmed that Jerusalem’s status must be resolved in peace negotiations between Israel and the Palestinians, Foreign Minister Penny Wong said.
+      
+      `,
   },
   {
     id: "6454",
     imageUrl: "/images/image-item2.jpg",
-    text: "nghsdgdfgdfagadsgfrtzt4etrewtertrewt4e64etzrehgsdrgdgdfsgdsfgdbfhrtehrhgrt",
+    text: `The Pentagon is considering paying for the Starlink satellite network — which has been a lifeline for Ukraine — from a fund that has been used to supply weapons and equipment over the long term, according to two U.S. officials who are involved in the deliberations.
+  
+      The Ukraine Security Assistance Initiative is designed to provide enduring support for the Ukrainian military by financing contracts with American firms for weapons and equipment that would be delivered in months or even years.
+      `,
   },
   {
     id: "5466",
     imageUrl: "/images/image-item4.jpg",
-    text: "bfdbadrgraegret4e6tr tztrze5rzterwterwterwteregradhraezrez",
+    text: `LONDON — In six short weeks, Liz Truss has succeeded in angering all wings of her party. Most now agree she can’t fight the next election.
+  
+      Britain’s latest prime minister, who won a Tory leadership contest with promises of tax cuts and “growth, growth, growth,” by Friday had driven supporters on the Tory right to send furious WhatsApp messages bemoaning her latest U-turn on corporation tax as more of her planned budget crumbled.
+      
+      “I’ve never known the atmosphere to be as febrile as it is at the moment,” one veteran Tory MP who backed Truss in the leadership contest said. Another MP who supported her said: “It feels like the end. I think she’ll be gone next week.”`,
   },
 ];
 
@@ -39,4 +50,5 @@ const StyledListContainer = styled.ul`
   padding-right: 2rem;
   list-style: none;
   padding-inline-start: 0;
+  overflow-wrap: break-word;
 `;

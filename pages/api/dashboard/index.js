164c0546bc59421f6830c1e27490de7efdea8e28
Thu Nguyen <thu_nguyen@gmx.de>
2022-11-10 23:39:23 +0100

    delete saved dashboards

--- pages/api/dashboard/index.js
+++ pages/api/dashboard/index.js
@@ -1,9 +1,13 @@
 import dbConnect from "../../../lib/dbConnect";
 import DashBoard from "../../../models/DashBoard";
+import { getAllDashBoards } from "../../../services/dashboardService";
 
 export default async function handler(request, response) {
   await dbConnect();
   switch (request.method) {
+    case "GET":
+      const allDashboards = await getAllDashBoards();
+      return response.status(200).json(allDashboards);
     case "POST":
       const dashboard = JSON.parse(request.body);
       const newDashBoard = await DashBoard.create(dashboard);
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,57 +7,6 @@ import { Icon } from "@iconify/react";
 import ColorBox from "../components/ColorBox";
 import { getAllSelections } from "../services/articleService";
 
-const draglist = [
-  {
-    id: "5343",
-    content:
-      "a quick 1% on the statement, assuming a newfound focus on “cumulative tightening” and “lags” signaled a shift in the committee’s thinking. Inklings of such a turn had already been offered by Fed officials, including Bullard, Daly, Evans and Kashkari. Markets could take comfort that the Fed was in sync with other central banks, with recent market instability forcin",
-    backgroundColor: "transparent",
-  },
-  {
-    id: "7565",
-    content:
-      "solve Paul Volcker had demonstrated some 40 years earlier. I respect and admire Powell. His Fed has made historic mistakes, but he’s determined to try to make amends. The Chair is doin",
-    backgroundColor: "transparent",
-  },
-  {
-    id: "5867",
-    content:
-      " want people to understand our commitment to getting this done. And to not making the mistake of not doing enough - or the mistake of withdrawing our strong policy and doing that",
-    backgroundColor: "transparent",
-  },
-  {
-    id: "1345",
-    content:
-      "ned or moved too fast. I think it’s been good and a successful program that we’ve gotten this far this fast. Remember though that we still think there’s a need for ongoing rate increases, a",
-    backgroundColor: "transparent",
-  },
-  {
-    id: "3671",
-    content:
-      "ay night (8 November) expecting a Republican landslide, as of Wednesday morning we still do not know who will control the Senate or even the House, which Republicans were supposed to win easily. Democrats held on in swing districts in Virginia; they even flipped some in Ohio. And while Florida looks lost from Democrats to Republicans for the foreseeable future, Pennsylvania (where John Fetterman won the Senate race), Wisconsin and Arizona, all of which Biden won in 2020, appear to be at the very least still in play for the party. The Democra",
-    backgroundColor: "transparent",
-  },
-  {
-    id: "9808",
-    content:
-      "use (the latter, at least, will still almost certainly be Republican; midterms are meant to be bad for the party in power). And we will undoubtedly hear competing explanations as to what happened. Some will say moderates were scared off by Donald Trump’s continued dominance of the Republican Party; others, that they lost because he wasn’t on the ballot. There will b",
-    backgroundColor: "transparent",
-  },
-  {
-    id: "0983",
-    content:
-      " reject political debate on right-wing terms. I’m going to take this opportunity to do the same: however Republicans try to spin the results, whatever nonsensical claims of fraud they draw up, their electoral offer was rejected by many who were expected to eagerly vote for it. They put up bad candidates and they ran extremist campaigns. They beat themselves. If they don",
-    backgroundColor: "transparent",
-  },
-  {
-    id: "9084",
-    content:
-      "Midterm elections are meant to be bad for the party in power. Midterms are definitely meant to be bad for the party in power when inflation and gas prices are high and the president isn’t popular with voters",
-    backgroundColor: "transparent",
-  },
-];
-
 const colorSet = [
   { id: "5454", color: "transparent" },
   { id: "87", color: "#E8E19C" },
@@ -79,7 +28,7 @@ export default function Dashboard({ allSelections }) {
   const dropzoneRef = useRef();
   const [draggableItems, setDraggableItems] = useState(allSelections);
   const [remainDragItemCount, setRemainDragItemCount] = useState(
-    draglist.length
+    allSelections.length
   );
   const [colorPalette, setColorPalette] = useState({});
   const [currentDraggable, setCurrentDraggable] = useState({

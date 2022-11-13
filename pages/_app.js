863549ff96a4bb31b7824a3d850510bbebc45962
Thu Nguyen <thu_nguyen@gmx.de>
2022-11-13 17:21:33 +0100

    include config for fontawesome in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import GlobalStyle from "../components/GlobalStyle";
 import Layout from "../components/Layout";
-
+//import "@fortawesome/fontawesome-svg-core/styles.css"; //importing font awesome css
+import { config } from "@fortawesome/fontawesome-svg-core";
 function MyApp({ Component, pageProps }) {
   config.autoAddCss = false;
   return (
